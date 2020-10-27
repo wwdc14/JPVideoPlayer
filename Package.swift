06f8d3fa51a75cae8a16bdd4dce433fa747145b2
hy <wwdc14@vip.qq.com>
2020-10-27 13:25:37 +0800

    update SPM

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "JPVideoPlayer",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -17,6 +17,8 @@ let package = Package(
             name: "JPVideoPlayer",
             path: "JPVideoPlayer",
             resources: [.process("JPVideoPlayer.bundle")],
-            cSettings: [.define("JP_USEING_SPM")])
+            publicHeadersPath: "JPVideoPlayer/include",
+            cSettings: [.define("JP_USEING_SPM")]
+            )
     ]
 )
